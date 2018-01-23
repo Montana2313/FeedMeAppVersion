6c4c0493bd9eb3b6660ed70df6ff2b9f54a21e29
Özgür Elmaslı <ozgurelmasli@Ozgur-iMac.local>
2018-01-23 21:09:33 +0300

    Initial Commit

---
+++ FeedMe/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  FeedMe
+//
+//  Created by Özgür  Elmaslı on 23.01.2018.
+//  Copyright © 2018 Özgür  Elmaslı. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
